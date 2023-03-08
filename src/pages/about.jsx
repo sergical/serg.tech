1f01b13c1b1a78208e6d91224bf8a8b9e398000a
Sergiy Dybskiy <hey@416serg.me>
2023-03-08 00:25:58 -0500

    yur

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,8 +1,4 @@
-import {
-  GitHubIcon,
-  LinkedInIcon,
-  TwitterIcon,
-} from '@/components/SocialIcons'
+import { GitHubIcon, LinkedInIcon, TwitterIcon } from '@/components/SocialIcons'
 
 import { Container } from '@/components/Container'
 import Head from 'next/head'
@@ -60,33 +56,70 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              I’m Sergiy Dybskiy. I live in London, Ontario, where I hack about and cuddle my goldendoodle, Luna 🐶.
+              I’m Sergiy Dybskiy. I live in London, Ontario, where I hack about
+              and cuddle my goldendoodle, Luna 🐶.
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
-                In Kharkiv, Ukraine, born and raised. On the soccer field is where I spent most of my days. Chillin&apos; out, hackin&apos;, relaxin&apos; all cool, all shooting some soccer balls outside of the school. 
+                In Kharkiv, Ukraine, born and raised. On the soccer field is
+                where I spent most of my days. Chillin&apos; out, hackin&apos;,
+                relaxin&apos; all cool, all shooting some soccer balls outside
+                of the school.
               </p>
               <p>
-                Then we moved to Canada 🇨🇦 and my older brother Yuriy encouraged me to pursue computer science. Tried that at University of Toronto for a bit but decided to continue on a self-taught path. Did a startup for 4 years. It has recently been aquired which is cool. 
+                Then we moved to Canada 🇨🇦 and when it came time to figuring out
+                what to do with my life, and the reason I am where I am today is
+                because of my brother, Yuriy. The mentorship, guidance, and
+                support he has given me has been invaluable. First, he
+                encouraged me to go to university and get a degree in Computer
+                Science. After studying for a year and putting that on pause, he
+                was there to provide an alternative path. He introduced me to
+                the tools, he connected me with people looking for work that
+                trusted him to give me a chance. So, broski, if you are reading
+                this, thank you for everything you have done for me. 💛
               </p>
               <p>
-                Proceeded to help my brother build an MVP of <Link href="https://pumabrowser.com/?ref=serg.tech" target="_blank" rel="noreferrer" className="text-teal-500">Puma Browser</Link> before joing Shopify and learning there for a few years. 
+                Did a startup for 4 years. It has recently been aquired which is
+                cool. Proceeded to help my brother build an MVP of{' '}
+                <Link
+                  href="https://pumabrowser.com/?ref=serg.tech"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-teal-500"
+                >
+                  Puma Browser
+                </Link>{' '}
+                before joing Shopify and learning there for a few years.
               </p>
 
               <p>
-                Today, I’m working at Coinbase on the Coinbase One team. We built a subscription product to give our users benefits like fee-free trading to help onboard into the web3 space easier and with more rewards. Think Amazon Prime but for Coinbase.
+                Today, I’m working at Coinbase on the Coinbase One team. We
+                built a subscription product to give our users benefits like
+                fee-free trading to help onboard into the web3 space easier and
+                with more rewards. Think Amazon Prime but for Coinbase.
               </p>
             </div>
           </div>
           <div className="lg:pl-20">
             <ul role="list">
-              <SocialLink href="https://twitter.com/sergiy_eth" icon={TwitterIcon}>
+              <SocialLink
+                href="https://twitter.com/sergiy_eth"
+                icon={TwitterIcon}
+              >
                 Follow on Twitter
               </SocialLink>
-              <SocialLink href="https://github.com/sergical" icon={GitHubIcon} className="mt-4">
+              <SocialLink
+                href="https://github.com/sergical"
+                icon={GitHubIcon}
+                className="mt-4"
+              >
                 Follow on GitHub
               </SocialLink>
-              <SocialLink href="https://linkedin.com/in/sergical" icon={LinkedInIcon} className="mt-4">
+              <SocialLink
+                href="https://linkedin.com/in/sergical"
+                icon={LinkedInIcon}
+                className="mt-4"
+              >
                 Follow on LinkedIn
               </SocialLink>
               <SocialLink
